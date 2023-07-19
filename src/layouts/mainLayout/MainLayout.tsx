e7bfad00451056737bcf19501be10dc4efe5e995
Jaison John <jaisonjohny26@gmail.com>
2023-07-19 16:13:46 +0530

    layout modified with footer bottom

--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -1,15 +1,16 @@
-import  { FC } from "react";
+import { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Footer } from "src/components/footer/Footer";
 import { Navbar } from "src/components/navbar/Navbar";
 
-
-export const MainLayout:FC = () => {
+export const MainLayout: FC = () => {
   return (
     <div className="flex flex-col min-h-screen">
-      <Navbar/>
-      <Outlet/>
-      <Footer/>
+      <Navbar />
+      <Outlet />
+      <div className="fixed bottom-0 w-full">
+        <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
